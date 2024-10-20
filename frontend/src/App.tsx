d9a5d696b5961b383609444050881aeb527ee9fa
flypoodles <lchen230@uic.edu>
2024-10-19 19:24:32 -0500

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+// object
+
+// type Person = {
+// name : string;
+// age? : number;  // ? means optional
+//}
+//
+
+const App: React.FC = () => {
+  return <div className="App">hello world</div>;
+};
+
+export default App;
