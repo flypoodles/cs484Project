4584cd821fae03210f976710cca59958a01b73da
flypoodles <lchen230@uic.com>
2024-11-27 21:17:03 -0600

    d

--- frontend/src/socket/socket.ts
+++ frontend/src/socket/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = "http://localhost:3000";
+const URL = "https://cs484project-2.onrender.com/";
 
 export const socket = io(URL, {
   autoConnect: false,
