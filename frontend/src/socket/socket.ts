f8c8e48b3c59fb3acb673babbc514c23a8cb744e
flypoodles <lchen230@uic.edu>
2024-12-06 03:02:37 -0600

    deploying

--- frontend/src/socket/socket.ts
+++ frontend/src/socket/socket.ts
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = "http://localhost:3000"; /*import.meta.env.VITE_SERVER_URL; */
+// "http://localhost:3000";
+const URL = import.meta.env.VITE_SERVER_URL;
 console.log(URL);
 export const socket = io(URL, {
   autoConnect: false,
