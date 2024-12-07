54eefde0f7513ca9a6940652141b359806b2f60b
Manh Phan <dphan8@uic.edu>
2024-12-06 18:14:54 -0600

    minor UI

--- backend/SocketEvents/SocketRoomLogic.ts
+++ backend/SocketEvents/SocketRoomLogic.ts
@@ -96,6 +96,7 @@ export const roomEvent = (
       socket.emit("Join Error", "There is no room to join")
       return
     }
+    console.log(availableRoom)
 
     // Add user to the room
     const roomNumber = availableRoom.roomNumber
